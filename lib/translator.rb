ef48647d7ad8ee77f9b9968730fb4d9673933666
Yining Wang <yining.wang@utexas.edu>
2019-10-25 19:53:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,22 @@
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(path)
+  hash = {}
+  require 'yaml'
+  file = YAML.load_file(path)
+  hash["get_meaning"] = {}
+  hash["get_emoticon"] = {}
+  file.each{|k, v| hash["get_meaning"][v[1]] = k}
+  file.each{|k, v| hash["get_emoticon"][v[0]] = v[1]}
+  hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emo)
+  hash = load_library(path)
+  hash['get_emoticon'].key?(emo) ? hash['get_emoticon'][emo] : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emo)
+  hash = load_library(path)
+  hash['get_meaning'].key?(emo) ? hash['get_meaning'][emo] : "Sorry, that emoticon was not found"
 end
